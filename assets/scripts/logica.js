4884692fa5984694e148723e30722524d75abe80
Pedro Lopez <plopez-2019280@kinal.edu.gt>
2024-01-29 20:24:05 -0600

    Subiendo estructura de los archivos

---
+++ assets/scripts/logica.js
@@ -0,0 +1,8 @@
+document.getElementById('login-btn').addEventListener('click', function () {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+
+    console.log('Usuario:', username);
+    console.log('Contraseña:', password);
+});
